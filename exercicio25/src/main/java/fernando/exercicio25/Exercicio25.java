4659d0ac18227e6bd91012e0af06a1ff1caf1eda
Fernando Alves <fernandoalves.dev@gmail.com>
2026-04-09 19:08:40 -0300

    Add files via upload

---
+++ exercicio25/src/main/java/fernando/exercicio25/Exercicio25.java
@@ -0,0 +1,28 @@
+
+package fernando.exercicio25;
+import java.util.Scanner;
+/**
+ *
+ * @author fernando
+ */
+public class Exercicio25 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // traduzir o exemplo em Portugol para Java
+        System.out.print("digite o nome de uma cidade: ");
+        String cidade = sc.next();
+        
+        // switch-case
+        switch (cidade) {
+            case "Jundiaí", "Santos", "Mauá", "Diadema" ->  {
+                System.out.println("é uma cidade de São Paulo.");
+            }
+            case "MonteVerde", "Camanducaia", "PousoAlegre" ->  {
+                System.out.println("é uma cidade de Minas Gerais");
+            }
+            default -> System.out.println("é uma cidade não listada no programa");
+        }
+    }
+}
